814ab96c5cab5dacd86c7f0e5acc78cd8fc253ea
Zehra Gul Buyukarslan <151182584+zehragulbuyukarslan@users.noreply.github.com>
2024-03-05 13:17:49 +0300

    Add files via upload

---
+++ 8. week square root of a number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	//extract the square root of entered number//
+	int n;
+	printf("\n enter a number: ");
+	scanf("%d",&n);
+	printf("\n square root of %d is: %.2f\n\n",n,sqrt(n));
+	return 0;
+}
